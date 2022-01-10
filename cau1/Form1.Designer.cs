5bc99c8706f7394b34b749ad8542ee8d41224686
Thẩm Nguyễn <thamminhnguyen321@gmail.com>
2022-01-10 17:09:30 +0700

    tao 3 lop

--- cau1/Form1.Designer.cs
+++ cau1/Form1.Designer.cs
@@ -29,24 +29,13 @@ namespace cau1
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(185, 139);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(744, 450);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -54,8 +43,6 @@ namespace cau1
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
 
